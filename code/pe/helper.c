4f5766f630ce64d87dc79614ff9835bd1117e76c
Abhishek <abhishekchoubey.oled@gmail.com>
2020-07-14 20:24:15 +0530

    NI

---
+++ code/pe/helper.c
@@ -0,0 +1,4 @@
+inline int multiple_of_x(const int x, const int num)
+{
+	return ((num % x) == 0);
+}
---
+++ code/pe/multiples.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static int multiple_of_x(const int x, const int num)
+{
+        return ((num % x) == 0);
+}
+
+static int multiple_of_x_and_y(const int x, const int y, const int num)
+{
+return 0;	
+}
+
+static int lcm(const int x, const int y)
+{
+	int grt = (x >= y) ? x : y;	
+}
+
+int main(void)
+{
+	return 0;
+}
